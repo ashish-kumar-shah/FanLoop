8009233e7ac743890e86bddb899a8f22dcd9dad9
Ashish kumar shah <ash.del.013@gmail.com>
2025-06-20 01:36:34 +0530

    update main jsx

--- client/src/Pages/Main.jsx
+++ client/src/Pages/Main.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import SideBar from '../Component/SideBar'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../Component/HomeElement/Navbar'
+import React from 'react';
+import SideBar from '../Component/SideBar';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Component/HomeElement/Navbar';
 import MobileTab from "../Component/MobileTabs";
 
 const Main = () => {
   return (
+    <div className="w-full h-screen flex flex-col">
+      {/* Fixed Navbar height */}
+      <div className="h-16 w-full shrink-0">
+        <Navbar />
+      </div>
 
+      <div className="w-full flex-1 flex overflow-hidden bg-gray-100">
+        {/* Sidebar (can be hidden on mobile) */}
+        <SideBar />
 
-    <div className="w-full h-screen flex flex-col gap-1">
-            <Navbar/>
-      <div className="w-full h-full p-0 m-0 bg-gray-100 flex  gap-0.5 overflow-hidden hide-scrollbar">
-   {/* sidebar */}
-   
-<SideBar/>
-  
+        {/* Main Outlet content */}
+        <div className="flex-1 overflow-y-auto hide-scrollbar pb-16 md:pb-0">
+          <Outlet />
+        </div>
+      </div>
 
-    <div className="box-2 h-full  w-full overflow-auto hide-scrollbar">
-      <Outlet/>
-<div className="h-1/4  w-full shrink-0 bg-white md:hidden"></div>
-
+      {/* Fixed MobileTab only for small screens */}
+      <div className="w-full h-16 fixed bottom-0 left-0 bg-white md:hidden border-t border-gray-200">
+        <MobileTab />
+      </div>
     </div>
+  );
+};
 
-   </div>
-   <div className="w-full h-fit flex justify-around items-center fixed bottom-0 p-1 bg-white md:hidden">
-              <MobileTab />
-            </div>
-    </div>
- 
-  )
-}
-
-export default Main
+export default Main;
